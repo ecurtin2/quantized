43ccc0a6b905402ba992e15fa0fe2d10c29de071
evan <evanmcurtin@gmail.com>
2019-11-22 14:16:09 -0600

    update version and try to fix

--- transit_chem/__init__.py
+++ transit_chem/__init__.py
@@ -2,4 +2,4 @@ from transit_chem.basis import *  # noqa F401
 from transit_chem.utils import *  # noqa F401
 from transit_chem.oneD import *  # noqa F401
 
-__version__ = "0.5.0"
+__version__ = "0.5.8"
